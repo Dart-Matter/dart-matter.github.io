8d1a89a22a9046b433ec2a13fc9af2007e7a82f3
Dart-Matter <92115705+Dart-Matter@users.noreply.github.com>
2024-01-21 00:18:29 -0800

    Add files via upload

---
+++ SensoredX/contents.js
@@ -0,0 +1,69 @@
+﻿
+var PAGE_FILE = 0;
+var PAGE_NAME = 1;
+var PAGE_HASCONTROLS = 2;
+var PAGE_LOADED = 3;
+
+var pages =
+[
+    // List all pages in order here.
+    
+    // Pages with "Has controls" will not accept gestures accept in a narrow band
+    // at the bottom of the screen.
+    
+    // File                 Name                            Has controls        Loaded
+    //                                                      (bypass gestures)
+    
+    ["cover.htm",           "Cover",                        0,                  0],
+    ["setup.htm",           "Setup",                        0,                  0],
+    ["using.htm",           "How To Use",                   0,                  0],
+    ["contents.htm",        "Contents",                     1,                  0],
+    
+    ["201402Title.htm",     "Feb 2014",                     0,                  0],
+    ["Shells.htm",          "Shells",                       0,                  0],
+    ["AntiqueLights.htm",   "Antique Lights",               0,                  0],
+    ["LongBeachStore.htm",  "Long Beach Store",             0,                  0],
+    ["Greeter.htm",         "Greeter",                      0,                  0],
+    ["BeachWood.htm",       "Beach Wood",                   0,                  0],
+    ["DamChain.htm",        "Dam Chain",                    0,                  0],
+    ["PipeOrgan.htm",       "Pipe Organ",                   0,                  0],
+    ["Guards.htm",          "Honor Guard",                  0,                  0],
+    ["SeattleBuilding.htm", "Seattle Building",             0,                  0],
+    ["Degobah.htm",         "Degobah",                      0,                  0],
+    ["LoneMushroom.htm",    "Lonely Mushroom",              0,                  0],
+    ["ChainHold.htm",       "Chain Hold",                   0,                  0],
+    ["Windmill.htm",        "Windmill",                     0,                  0],
+    ["EbeyLightHouse.htm",  "Lighthouse",                   0,                  0],
+    ["HotelDel.htm",        "Hodel Del",                    0,                  0],
+    
+    ["FloraTitle.htm",      "Flora",                        0,                  0],
+    ["BirdsOfParadise.htm", "Birds-of-paradise",            0,                  0],
+    ["PinkFlowers.htm",     "Pink Flowers",                 0,                  0], 
+    ["BalboaLilies.htm",    "Water Lilies",                 0,                  0],
+    ["PitcherPlant.htm",    "Pitcher Plant",                0,                  0],
+    ["BurgundyOrchid.htm",  "Burgundy Orchid",              0,                  0],
+    ["SlipperOrchid.htm",   "Slipper Orchid",               0,                  0],
+    ["YellowOrchid.htm",    "Yellow Orchid",                0,                  0],
+    ["SpoonDaisy.htm",      "Spoon Daisy",                  0,                  0],
+    ["SpanishFlag.htm",     "Spanish Flag",                 0,                  0],
+    ["LeafDrop.htm",        "Leaf Drop",                    0,                  0],
+    
+    ["FlightTitle.htm",     "Flight",                       0,                  0],
+    ["A6.htm",              "EA-6B Prowler",                0,                  0],
+    ["A10.htm",             "A-10 Thunderbolt",             0,                  0],
+    ["BlueAngel.htm",       "Blue Angel",                   0,                  0],
+    ["Kites.htm",           "Kites",                        0,                  0],
+    ["787.htm",             "Boeing 787",                   0,                  0],
+    ["Butterfly.htm",       "Butterfly",                    0,                  0],
+
+    ["SanDiego.htm",        "Places",                       0,                  0],
+    ["CoronadoBeach.htm",   "Coronado Beach",               0,                  0],
+    ["LakeUnion.htm",       "Seattle from Lake Union",      0,                  0],
+    ["GasWorksPark.htm",    "Gas Works Park",               0,                  0],
+    ["DeadTrees.htm",       "Dead Trees",                   0,                  0],
+    ["ForestWalk.htm",      "Forest Walk",                  0,                  0],
+    ["WhiteHouseWatch.htm", "Watching from White House",    0,                  0],
+    ["CapitalKids.htm",     "Kids at the Capital",          0,                  0],
+     
+    ["TheEnd.htm",          "The End",                      0,                  0],
+];
